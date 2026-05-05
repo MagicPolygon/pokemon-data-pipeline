5e75b587ccb9da95de3f6448dc0e974245e0b64f
Ivan Quartey <IvanQuartey@hotmail.co.uk>
2026-05-05 15:15:54 +0100

    Created method of adding names to pokemon data dictionary

---
+++ pipeline_script.py
@@ -0,0 +1,57 @@
+import json
+import requests
+
+# pokemon_request_json = requests.get("https://pokeapi.co/api/v2/pokemon/turtwig").json()
+# pokemon_species_request_json = requests.get("https://pokeapi.co/api/v2/pokemon-species/turtwig/").json()
+
+# print("pokemon_request_json:")
+# print(pokemon_request_json.keys())
+# print("pokemon_species_request_json:")
+# print(pokemon_species_request_json.keys())
+
+# pokemon = "giratina"
+# pokemon_request_status = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}").status_code
+# print(pokemon_request_status)
+# pokemon_request_json = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}/").json()
+# print(pokemon_request_json)
+# print("Types:")
+# print(pokemon_request_json["types"])
+# types = pokemon_request_json["types"]
+# print("Type type:")
+# print(type(pokemon_request_json["types"]))
+# for pokemon_type in types:
+#     print(pokemon_type["type"]["name"])
+
+pokemon_list = ["torterra", "dialga", "gliscor", "dusknoir", "bronzong"]
+pokemon_data_dict = {}
+
+for pokemon in pokemon_list:
+    pokemon_data = {}
+    pokemon_data_dict.update({pokemon: pokemon_data})
+
+    # Inserting pokemon name
+    print(f"Pokemon Name: {pokemon}")
+    pokemon_data_dict[pokemon]["name"] = pokemon
+
+
+    # pokemon_request_json = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}/").json()
+    # types = []
+    # for pokemon_type in pokemon_request_json["types"]:
+    #     print(pokemon_type["type"]["name"])
+    #     types.append(pokemon_type["type"]["name"])
+
+print(pokemon_data_dict)
+
+
+
+
+        # types.append(pokemon_type["type"]["name"])
+    # pokemon_data.update({pokemon: pokemon_request_json["types"]})
+    # pokemon_types =
+    # print(f"Pokemon Types: {pokemon_types}")
+
+# print(pokemon_data)
+
+# pokemon_data =
+
+# Is giratina not on here?
